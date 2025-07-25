49bbd097565c61d291456a4dbb2026c8c10d48e6
lumishka <mkuk3592@gmail.com>
2025-07-25 21:20:24 +0530

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+//console.log("2" > 1);
+//console.log("02" > 1);
+
+//console.log(null > 0);
+//console.log(null == 0);
+//console.log(null >= 0); // equality check (==) and comparisons (<,>,<=,>=) work differently. Comparisons convert null to a number, 
+// treating it as 0. That's why null==0 is false but null>=0 is true.
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
+console.log(undefined <= 0);
+console.log(undefined >= 0);
+
+
+// === stands for triple check. It not just checks the value but also the datatypes
+console.log(undefined === 0);
+
