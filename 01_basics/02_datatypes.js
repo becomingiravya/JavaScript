bb039c5d3c6c3ce145eb36662b680dbf724f8ef1
lumishka <mkuk3592@gmail.com>
2025-07-25 09:48:09 +0530

    datatypes leant

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as  newer version
+
+//alert(3+3);  //we are using nodejs not browser
+
+console.log(2
+    +3) //don't do this code readability should be high
+
+    let name = "lumishka"
+    let age = 19 
+    let isLoggedIn = false
+    let state = null;
+
+    // number => 2 to power of 53
+    // bigint
+    // string => ""
+    // boolean => true/false
+    // null => standalone value
+    // undefined =>
+    // symbol => uniqueness
+
+
+
+    // object
+
+    console.log(typeof "lumishka")
+    console.log(typeof age)
+    console.log(typeof null)       //object
+    console.log(typeof undefined)  //undefined
