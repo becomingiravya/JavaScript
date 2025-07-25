1d85f919df9dc8ee9a6ff89de52d223d1e07bce5
lumishka <mkuk3592@gmail.com>
2025-07-25 10:08:08 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,37 @@
+//let score = 44
+//let score = "44"
+//let score = "44abc"
+//let score = null
+//let score = undefined
+let score = true
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber);
+
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1; false => 0
+
+
+//let isLoggedIn = 1
+//let isLoggedIn = ""
+let isLoggedIn = "lumishka"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "lumishka" => true
+
+
+let someNumber = 44
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
