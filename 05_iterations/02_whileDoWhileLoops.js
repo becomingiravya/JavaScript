00e2be6260753a197e2cc6f10bb9bb1a61ddf3b2
lumishka <mkuk3592@gmail.com>
2025-11-01 01:41:46 +0530

    while and do while loop

---
+++ 05_iterations/02_whileDoWhileLoops.js
@@ -0,0 +1,52 @@
+
+// WHILE LOOP 
+
+//SYNTAX 
+
+// let index = 0
+// while(condition){
+// console.log();
+// index = index + 2; 
+// }
+
+
+let index = 0 
+while(index<=10){
+    //console.log(`Value of index is ${index}`);
+    index = index + 2;
+}
+
+let myHeroes  = ["flash", "batman", "superman"]
+
+
+let i = 0;
+while(i < myHeroes.length){
+    // console.log(`Value is ${myHeroes[i]}`);
+    i = i+ 1;
+}
+
+
+
+// DO WHILE LOOP
+
+// SYNTAX   
+
+// do {
+
+// }
+// while(condition);
+
+
+// let score  = 1
+
+// do{
+//     console.log(`score is ${score}`);
+//     score++;
+// } while(score<=10)
+
+let score  = 11
+
+do{
+    console.log(`score is ${score}`);
+    score++;
+} while(score<=10)
